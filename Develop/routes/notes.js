2a1a6c3d957ad949c5b852f00e4fdf72239a5c99
Steven Gray <stevegray@granite-mtn.com>
2023-11-25 23:22:26 -0700

    Removed console.log and console.info messages from notes.js

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -8,16 +8,11 @@ const writeFile = util.promisify(fscb.writeFile);
 const readFile = util.promisify(fscb.readFile);
 // GET Route for retrieving all the notes
 notes.get('/', (req, res) => {
-    console.info(`${req.method} request received for notes`);
-    console.log(`request received for notes`);
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
-    console.log("After read from file");
   });
   
   // POST Route for a new note
   notes.post('/', (req, res) => {
-    console.info(`${req.method} request received to add a note`);
-  
     const { title, text } = req.body;
   
     if (title && text) {
@@ -35,8 +30,6 @@ notes.get('/', (req, res) => {
   });
   // Delete a note by ID
 notes.delete('/:noteId', async (req, res) => {
-  console.info(`${req.method} request received to delete a note`);
-  console.log("notes.delete req.params.noteId = " + req.params.noteId)
   const noteIdToDelete = req.params.noteId;
 
   try {
@@ -59,7 +52,6 @@ notes.delete('/:noteId', async (req, res) => {
       res.status(404).json(`Note with ID ${noteIdToDelete} not found`);
     }
   } catch (error) {
-    console.error('Error deleting note:', error);
     res.status(500).json('Internal Server Error');
   }
 });
